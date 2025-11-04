e5e802dbdea2fb635e8de12f62e0300be31e124e
gwenael9 <gwenaelgueho@gmail.com>
2025-11-04 15:05:35 +0100

    interface

--- main.go
+++ main.go
@@ -1,25 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+type Notifier interface {
+	Send(message string) error
+}
+
+type EmailNotifier struct {
+	RecipientEmail string
+}
+
+func (e EmailNotifier) Send(message string) error {
+	fmt.Printf("Envoi de l'email à %s : %s\n", e.RecipientEmail, message)
+	return nil
+}
+
+type SmsNotifier struct {
+	PhoneNumber string
+}
+
+func (s SmsNotifier) Send(message string) error {
+	if !strings.HasPrefix(s.PhoneNumber, "06") {
+		return fmt.Errorf("numéro invalide : %s", s.PhoneNumber)
+	}
+	fmt.Printf("Envoi du SMS au %s : %s\n", s.PhoneNumber, message)
+	return nil
+}
 
 func main() {
-	var lang language = "fr"
-	greeting := greet(lang)
-	fmt.Println(greeting)
-}
-
-type language string
-
-var phraseBook = map[language]string{
-	"fr": "Bonjour",
-	"en": "Hello",
-	"es": "Hola",
-}
-
-func greet(l language) string {
-	greeting, ok := phraseBook[l]
-	if !ok {
-		return fmt.Sprintf("Unsupported language: %q", l)
+	notifToSend := []Notifier{
+		EmailNotifier{RecipientEmail: "toto@mail.com"},
+		SmsNotifier{PhoneNumber: "0622334455"},
+		EmailNotifier{RecipientEmail: "toto2@mail.com"},
+		SmsNotifier{PhoneNumber: "0522334455"},
 	}
-	return greeting
+
+	message := "Mise à jour"
+
+	for _, notifier := range notifToSend {
+		err := notifier.Send(message)
+		if err != nil {
+			fmt.Println("Erreur :", err)
+		}
+	}
 }
